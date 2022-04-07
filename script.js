1b0fb3ca68c7f24755fa77a1247939415768dd47
Tomás Caraballo (Yomi) <yomizana@gmail.com>
2022-04-07 15:29:12 -0400

    Make calculator able to do simple arithmetic operations

--- script.js
+++ script.js
@@ -1,22 +1,47 @@
 const span = document.querySelector('span');
 const numberButtons = document.querySelectorAll('.number');
 const operatorButtons = document.querySelectorAll('.operator');
+const equalsButton = document.querySelector('.equals');
 const clearButton = document.querySelector('.clear');
+
+let operation = {
+    firstNumber: 0,
+    operator: '',
+    secondNumber: 0,
+    result: 0
+}
+
+let displayScreenData = '';
 
 numberButtons.forEach(button => {
     button.addEventListener('click', () => {
         span.textContent += button.value;
+        displayScreenData = parseInt(span.textContent);
     });
 });
 
 operatorButtons.forEach(button => {
     button.addEventListener('click', () => {
+        operation.firstNumber = displayScreenData;
+        displayScreenData = '';
+
         span.textContent += button.value;
+        operation.operator = button.value;
     });
+});
+
+equalsButton.addEventListener('click', () => {
+    operation.secondNumber = displayScreenData;
+    displayScreenData = '';
+
+    operate(operation);
+    span.textContent = operation.result;
 });
 
 clearButton.addEventListener('click', () => {
     span.textContent = "";
+
+    resetOperation();
 });
 
 function add(a, b) {
@@ -35,9 +60,9 @@ function divide (a, b) {
     return a / b;
 }
 
-function operate (operator, a, b) {
-    if (operator === '+') return add(a, b);
-    if (operator === '-') return subtract(a, b);
-    if (operator === '*') return multiply(a, b);
-    if (operator === '/') return divide(a, b);
+function operate (obj) {
+    if (obj.operator === '+') obj.result = add(obj.firstNumber, obj.secondNumber);
+    if (obj.operator === '-') obj.result = subtract(obj.firstNumber, obj.secondNumber);
+    if (obj.operator === '*') obj.result = multiply(obj.firstNumber, obj.secondNumber);
+    if (obj.operator === '/') obj.result = divide(obj.firstNumber, obj.secondNumbers);
 }
