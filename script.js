6e3a3489058b6273bb3d731a8a6de7b219c86149
Tomás Caraballo (Yomi) <yomizana@gmail.com>
2022-04-12 05:44:06 -0400

    Refactor code so it's easier to understand

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const span = document.querySelector('span');
+const operationArea = document.querySelector('.operation');
+const resultArea = document.querySelector('.result');
 const numberButtons = document.querySelectorAll('.number');
 const operatorButtons = document.querySelectorAll('.operator');
 const decimalButton = document.querySelector('.decimal');
@@ -6,122 +7,151 @@ const equalsButton = document.querySelector('.equals');
 const clearButton = document.querySelector('.clear');
 const backButton = document.querySelector('.back');
 
-let displayData = '';
-let operatorActive = false;
+operationArea.textContent = '';
+resultArea.textContent = '0';
 
-backButton.addEventListener('click', () => {
-    span.textContent = span.textContent.slice(0, -1);
-    displayData = displayData.slice(0, -1);
-});
+let firstNumber = '';
+let secondNumber = '';
+let operator = '';
+let result = '';
 
 numberButtons.forEach(button => {
-    button.addEventListener('click', () => {
-
-        span.textContent += button.value;
-        displayData += button.value;
-
-    });
+    button.addEventListener('click', () => appendNumber(button.value))
 });
 
 operatorButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        if (operatorActive) {
-            let result = getResult();
-            showResult(result);
+    button.addEventListener('click', () => setOperator(button.value));
+});
+
+decimalButton.addEventListener('click', () => appendDecimal(decimalButton.value));
+equalsButton.addEventListener('click', () => evaluateOperation());
+backButton.addEventListener('click', () => removeLastValue());
+clearButton.addEventListener('click', () => resetOperation());
+
+function appendNumber(value) {
+
+    operationArea.textContent += value;
+
+    if (!operator) {
+        firstNumber += value;
+        
+    } else {
+        secondNumber += value;
+    }
+}
+
+function appendDecimal(value) {
+
+    if (!operator) {
+        if (includesDecimal(firstNumber)) return;
+
+        operationArea.textContent += value;
+        firstNumber += value;
+        
+    } else {
+        if (includesDecimal(secondNumber)) return;
+
+        operationArea.textContent += value;
+        secondNumber += value;
+    }
+}
+
+function includesDecimal(number) {
+
+    if (number.includes('.')) return true;
+    return false;
+
+}
+
+function setOperator(value) {
+    if (firstNumber === '') return;
+
+    if (operator && secondNumber) {
+        evaluateOperation();
+        operationArea.textContent = result;
+
+        operator = value;
+
+        operationArea.textContent += operator;
+
+        return;
+    }
+    
+    if (operator) {
+        
+        if (result) operationArea.textContent = result;
+
+        operator = value;
+
+        if (isLastOperator()) {
+            operationArea.textContent = operationArea.textContent.slice(0, -1);
         }
 
-        setOperator(button);
-        operatorActive = true;
-    });
-});
+        operationArea.textContent += operator;
 
-decimalButton.addEventListener('click', () => {
-    displayData += decimalButton.value;
-    span.textContent += decimalButton.value;
-});
-
-equalsButton.addEventListener('click', () => {
-    let result = getResult();
-    showResult(result);
-    operatorActive = false;
-});
-
-clearButton.addEventListener('click', () => {
-
-    resetOperation();
-    
-});
-
-function setOperator(button) {
-    switch (button.value) {
-        case '+':
-            displayData += 'add';
-            span.textContent += '+'
-            break;
-        case '-':
-            displayData += 'subtract';
-            span.textContent += '-'
-            break;
-        case '*':
-            displayData += 'multiply';
-            span.textContent += 'x'
-            break;
-        case '/':
-            displayData += 'divide';
-            span.textContent += '÷';
-            break;
-    }
-}
-
-function getResult() {
-    const numbers = parseNumbers(displayData);
-    const operator = parseOperator(displayData);
-
-    let result = operate(numbers[0], operator, numbers[1]);
-
-    let resultLength = result.toString().length;
-
-    if (resultLength > 15) {
-        result = Math.round((result + Number.EPSILON) * (10**15)) / (10**15);
-    }
-
-    return result;
-}
-
-function showResult(result) {
-    displayData = result.toString();
-    span.textContent = result.toString();
-}
-
-function parseNumbers(displayData) {
-    const regEx = /(add|subtract|multiply|divide)+/;
-
-    let numbers = displayData.replace(regEx, ',').split(',');
-
-    if (!isInputValid(numbers)) {
         return;
     }
 
-    numbers = numbers.map(number => Number(number));
-
-    return numbers;
+    operationArea.textContent += value;
+    operator = value;
 }
 
-function isInputValid(numbers) {
-    if (numbers[0] === '' || numbers[1] === '') {
-        span.textContent = 'SYNTAX ERROR';
-        return false;
+function isLastOperator() {
+    const text = operationArea.textContent.slice(-1);
+    const operators = ['+', '-', '*', '/'];
+
+    for (let i = 0; i < operators.length; i++) {
+        if (text === operators[i]) return true;
+    }
+
+    return false;
+
+}
+
+function evaluateOperation() {
+    if (!secondNumber) return alert('Invalid operation.');
+    firstNumber = Number(firstNumber);
+    secondNumber = Number(secondNumber);
+
+    if (operator === '/' && secondNumber === 0) {
+        return alert('You can\'t divide by zero.');
+    }
+
+    result = operate(firstNumber, operator, secondNumber);
+
+    result = Math.round(result * 10000) / 10000;
+
+    console.log(result);
+
+    resultArea.textContent = result;
+
+    firstNumber = result.toString();
+    secondNumber = '';
+    operationArea.textContent = '';
+}
+
+function removeLastValue() {
+    if (secondNumber) {
+        operationArea.textContent = operationArea.textContent.slice(0, -1);
+        secondNumber = secondNumber.slice(0, -1);
+        return;
     } 
-    return true;
-}
 
-function parseOperator(displayData) {
-    const regEx = /(add|subtract|multiply|divide)/;
+    if  (operator) {
+        operationArea.textContent = operationArea.textContent.slice(0, -1);
+        operator = '';
+        return;
+    } 
+    
+    if (firstNumber) {
+        operationArea.textContent = operationArea.textContent.slice(0, -1);
+        firstNumber = firstNumber.slice(0, -1);
 
-    const match = displayData.match(regEx);
-    const operator = match[0];
+        if (result) result.slice(0, -1);
+        return;
+    }
 
-    return operator;
+    return;
 }
 
 function add(a, b) {
@@ -141,14 +171,18 @@ function divide (a, b) {
 }
 
 function operate (firstNumber, operator, secondNumber) {
-    if (operator === 'add') return add(firstNumber, secondNumber);
-    if (operator === 'subtract') return subtract(firstNumber, secondNumber);
-    if (operator === 'multiply') return multiply(firstNumber, secondNumber);
-    if (operator === 'divide') return divide(firstNumber, secondNumber);
+    if (operator === '+') return add(firstNumber, secondNumber);
+    if (operator === '-') return subtract(firstNumber, secondNumber);
+    if (operator === '*') return multiply(firstNumber, secondNumber);
+    if (operator === '/') return divide(firstNumber, secondNumber);
 }
 
 function resetOperation() {
-    displayData = '';
-    span.textContent = '';
-    operatorActive = false;
+    firstNumber = '';
+    secondNumber = '';
+    operator = '';
+    result = '';
+
+    operationArea.textContent = '';
+    resultArea.textContent = '0';
 }
